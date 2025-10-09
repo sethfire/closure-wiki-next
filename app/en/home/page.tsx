e9b6d64bba2251a3a47b74a6a6486c24e55c6927
Sethfire <17826826+Sethfire@users.noreply.github.com>
2025-10-09 15:23:40 +1100

    Todo

--- app/en/home/page.tsx
+++ app/en/home/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col gap-4 w-full px-4 md:px-0">
-      <div className="mb-8">
+      <div>
         <h2 className="text-lg font-semibold mb-2">Welcome!</h2>
         <p>closure.wiki is an unofficial resource archive for Arknights. This is still a work in progress, so some pages may be incomplete.</p>
         <p>Disclaimer: This is a hobby project and is not affiliated with Hypergryph or Yostar. All trademarks are property of their respective owners.</p>
@@ -19,6 +19,15 @@ export default function Page() {
           className="max-w-md md:max-w-xl w-full h-auto"
         />
       </div> */}
+      <div>
+        <h2 className="text-lg font-semibold mb-2">Todo stuff</h2>
+        <ul className="list-disc pl-5">
+          <li>Search functionality</li>
+          <li>Sorting and filtering on lists</li>
+          <li>Table of contents on entry pages</li>
+          <li>Multiple language support</li>
+        </ul> 
+      </div>
 
       <div>
         <h2 className="text-lg font-semibold mb-2">Navigation</h2>
